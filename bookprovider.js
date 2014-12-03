a813d7a68509befa97836b505ef78b66b77ba035
weno42 <obroberts@gmail.com>
2014-12-02 23:36:45 -0500

    .open error

--- bookprovider.js
+++ bookprovider.js
@@ -9,7 +9,7 @@ var mongodb = require('mongodb'),
 BookProvider = function(uri) {
   mongodb.MongoClient.connect(uri, { server: { auto_reconnect: true } }, function (error, database) {
     this.db = database;
-    this.db.open(function(){});
+    //this.db.open(function(){});
   });
   //this.db = new Db('books', new Server(uri, {safe: false}, {auto_reconnect: true}, {}));
   //this.db.open(function(){});
